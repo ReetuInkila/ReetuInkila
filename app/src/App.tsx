f8d082ec997fc5c28b667aeec635ddb7d6a9f002
lrtinkiv <lrtinkiv@jyu.fi>
2024-02-01 20:02:53 +0200

    content added

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -4,7 +4,32 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1>How Did You Find Here?</h1>
+      <p>
+        Greetings! I'm Reetu, a passionate physics student who's decided to take a leap into 
+        the world of software development. While my academic roots lie in the complexities of 
+        physics, my heart and curiosity have led me to the ever-evolving realm of web 
+        development.
+      </p>
+      <p>
+        While I'm comfortable with several programming languages, the easiest way to get in 
+        touch with me is through the languages JavaScript, C#, and Python.
+      </p>
+      <p>
+        Curious about how I spend my free time? Take a sneak peek into my latest run 
+        <a href="https://syke.inkilareetu.fi/">here</a>. If you're feeling a bit more 
+        adventurous, you can indulge in some "temperature espionage" and check out the 
+        current climate in our home <a href="http://reetuinkila.eu.pythonanywhere.com/">here</a>.
+      </p>
+      <div id="links">
+        <a href="https://www.linkedin.com/in/reetuinkila/">LinkedIn</a> 
+        <a href="https://github.com/ReetuInkila">Github</a>
+        <a href="https://www.strava.com/athletes/40032012">Strava</a>
 
+      </div>
+      <p>
+        JavaScript | C++ | Python | Jupyter Notebook | Firebase | GCP | SQL | Docker | CSS | TypeScript | HTML | Git | Java | React | Flask | Arduino | C#
+      </p>
     </div>
   );
 }
